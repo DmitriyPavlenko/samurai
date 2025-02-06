255deb0e44b9cecb122645c5784799fa23e09fcf
Dmitriy Pavlenko <pavlenko.d.o@gmail.com>
2025-02-06 19:09:15 +0200

    commit-5

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./Profile/MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <img src="https://piligrim.ua/wp-content/uploads/2021/08/Kendwa.jpg" alt=""/>
             <div>ava+description</div>
             <MyPosts hey='yo'/>
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,7 @@ const Post = (props) => {
     return (
         <div className={s.item}>
             <img
-                src='https://lumiere-a.akamaihd.net/v1/images/a_avatarpandorapedia_jakesully_16x9_1098_02_b13c4171.jpeg?region=340%2C0%2C1081%2C1081'/>
+                src='https://lumiere-a.akamaihd.net/v1/images/a_avatarpandorapedia_jakesully_16x9_1098_02_b13c4171.jpeg?region=340%2C0%2C1081%2C1081' alt=""/>
             {props.message}
             <div>like</div>
         </div>
